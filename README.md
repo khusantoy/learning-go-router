dccd2ca8dbf25c5450be0f7c8ae8a99faeba1826
Khusanboy Tursunov <tursunovkhusanboy@gmail.com>
2024-08-12 23:07:43 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# learning_go_router
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/learning_go_router/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.learning_go_router
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
